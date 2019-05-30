785e7c01288382c9d3da59adee712faa13140973
Eugene Uliganets <jenia.uliganets@gmail.com>
2019-05-30 18:47:49 +0200

    adding latest photo in main/ adding summer and autumn

---
+++ app/src/main/java/com/example/greety/AutumnBackgrounds.java
@@ -0,0 +1,13 @@
+package com.example.greety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AutumnBackgrounds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autumn_backgrounds);
+    }
+}
